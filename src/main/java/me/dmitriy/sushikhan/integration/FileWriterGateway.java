c715ab545756bb54a144f0056cbba55f7b2b34e8
DmitryRoschupkin <dmitrii.roshupkin@gmail.com>
2026-07-20 13:56:37 +0500

    defined simple spring integration flow from writing in files

---
+++ src/main/java/me/dmitriy/sushikhan/integration/FileWriterGateway.java
@@ -0,0 +1,14 @@
+package me.dmitriy.sushikhan.integration;
+
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "textInChannel")
+public interface FileWriterGateway {
+
+    void writeToFile(
+            @Header(FileHeaders.FILENAME) String filename, String data
+            );
+}
---
+++ src/main/java/me/dmitriy/sushikhan/integration/FileWriterIntegrationConfig.java
@@ -0,0 +1,31 @@
+package me.dmitriy.sushikhan.integration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.core.GenericTransformer;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
+import org.springframework.integration.file.outbound.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+
+import java.io.File;
+
+@Configuration
+public class FileWriterIntegrationConfig {
+
+    @Bean
+    public IntegrationFlow fileWriterFlow() {
+        return IntegrationFlow
+                .from(MessageChannels.direct("textInChannel"))
+                .<String, String>transform(String::toUpperCase)
+                .handle(Files
+                        .outboundAdapter(new File("/files"))
+                        .fileExistsMode(FileExistsMode.APPEND)
+                        .appendNewLine(true))
+                .get();
+
+    }
+}
